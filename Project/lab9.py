c02b26827933b2e57b44ef710021f075ec78271f
SaqibSyed-prog <xsyedx0@gmail.com>
2021-11-25 19:54:40 -0500

    check about us page

--- Project/lab9.py
+++ Project/lab9.py
@@ -1,21 +1,12 @@
 import random
-n = 0
-arr = []
-
-while n < 10:
-    n += 1
-    num = random.randint(0, 100)
-    print(num)
-    arr.append(num)
-
-print("Here are the numbers if any: ")
-
 i = 0
-for x in arr:
-    temp = x % 11
-    i += 1
-    if temp == 0:
-        print(str(i))
-        print(str)
+temp = 100
 
+while i < 10: 
+    i += 1 
+    num = random.randint(0, 100)
+    print (num)
+    if num < temp:
+        temp = num
+print ("smallest number: ", temp)
 
